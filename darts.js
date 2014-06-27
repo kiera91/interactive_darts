ebbcf9fe3c79757382d7f386332fd0c24ccb0043
Kiera <kforeilly7@hotmail.com>
2014-06-27 15:29:13 +0100

    adding players to table

--- darts.js
+++ darts.js
@@ -2,6 +2,33 @@ $(document).ready(function(){
 	$(".ringTwo").click(function(e){
 
 		var target = $(e.target);
-		console.log(target.parents())
+	});
+
+	$('#popupBoxClose').click( function() {
+		unloadPopupBox();
 	});
 });
+
+function showNewPlayer(){
+	loadPopupBox();
+}
+
+function registerNewPlayer(){
+	var playerName = $("#name").val();
+	$('.gridtable').append('<tr><td>' + playerName + '</td><td>1</td><td>0</td></tr>');
+}
+
+function unloadPopupBox() {    // TO Unload the Popupbox
+    $('#popup_box').fadeOut("slow");
+    $("#container").css({ // this is just for style        
+            "opacity": "1"
+    });
+}
+    
+function loadPopupBox() {    // To Load the Popupbox
+    $('#popup_box').fadeIn("slow");
+    $("#container").css({ // this is just for style
+        "opacity": "0.3"
+    });
+}
+
